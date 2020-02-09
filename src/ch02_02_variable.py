0c3794390f98930557f01eea5acfb40ca8a7cd30
whee <wheejinv@naver.com>
2020-02-09 15:07:07 +0900

    파이썬 변수 https://www.inflearn.com/course/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%9E%85%EB%AC%B8-%EC%9D%B8%ED%94%84%EB%9F%B0-%EC%98%A4%EB%A6%AC%EC%A7%80%EB%84%90/lecture/22361

---
+++ src/ch02_02_variable.py
@@ -0,0 +1,15 @@
+# 파이썬
+
+# 기본 선언
+m = n = 700
+
+print(n)  # 700
+print(type(n))  # <class 'int'>
+
+
+# Object References
+# 타입에 맞는 오브젝트 생성
+print(id(n))  # 4347363792
+print(id(m))  # 4347363792
+m = 300
+print(id(m))  # 4564092032 (따로 대입을 해야 id가 갈림)
